62268158e4e6e91d45ed313a0c417e53ae956d11
KilicerDev <ertugul.kilic@icloud.com>
2026-08-20 00:23:38 +0200

    chore: ios native app design projects and tasks, no backend wired up yet

---
+++ apps/trackr-mobile-ios/trackr-mobile-ios/trackr_mobile_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackr_mobile_iosApp.swift
+//  trackr-mobile-ios
+//
+//  Created by Ertugul Kilic on 19.08.26.
+//
+
+import SwiftUI
+
+@main
+struct trackr_mobile_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
